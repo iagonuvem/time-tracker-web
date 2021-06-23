0f28e2cb14d96a47064b864c9fb7182fddaab706
Iago Nuvem <iagonuvem@hotmai.com>
2021-06-23 17:29:57 -0300

    Organizando diretorios

--- src/app/members/private/home/home.module.ts
+++ src/app/members/private/home/home.module.ts
@@ -12,7 +12,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { TimeTrackerComponent } from '../../timetracker/timetracker.component';
+import { TimeTrackerComponent } from '../../modals/timetracker/timetracker.component';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDialogModule } from '@angular/material/dialog';
