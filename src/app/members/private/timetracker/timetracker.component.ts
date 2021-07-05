3dbe3d1f272527408ed9b06436f07758a4b33bb2
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-05 00:50:30 -0300

    Form já minimamente apresentável

--- src/app/members/private/timetracker/timetracker.component.ts
+++ src/app/members/private/timetracker/timetracker.component.ts
@@ -1,7 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { FocalPoint } from 'src/app/models/FocalPoint';
+import { Project } from 'src/app/models/Project';
+import { TaskCategory } from 'src/app/models/TaskCategory';
 import { TaskDescription } from 'src/app/models/TaskDescription';
 import { TimeTrack } from 'src/app/models/TimeTrack';
+import { ProjectService } from 'src/app/services/project-service.service';
+import { TaskCategoryService } from 'src/app/services/task-category-service.service';
+import { TaskDescriptionService } from 'src/app/services/task-description-service.service';
 import { TimeTrackService } from '../../../services/time-track.service';
 
 
@@ -9,33 +16,42 @@ import { TimeTrackService } from '../../../services/time-track.service';
   selector: 'time-tracker-component',
   templateUrl: './timetracker.component.html',
   styleUrls: ['./timetracker.component.scss'],
-  // providers: [ MatOption ],
 })
 export class TimeTrackerComponent implements OnInit {
-  group :FormGroup;
+  group: FormGroup;
   _model: TimeTrack;
-  project!: string;
-  taskDescription!:string;
-  taskCategory!: string;
-  taskDescriptions: TaskDescription[] = [
-    {id: 1, description: 'Pretending you care'},
-    {id: 1, description: 'Procrastinating '},
-    {id: 1, description: 'Using compiling time as an excuse to slack off'},
-    {id: 1, description: 'Paid poop time'},
-    {id: 1, description: 'Inserting new bugs to ensure you\'ll always have a job'},
-  ];
-  constructor(public service: TimeTrackService, public fb: FormBuilder) {
-    this._model = { } as TimeTrack;
+  _focalPoints = new BehaviorSubject<Array<FocalPoint>>([]);
+  // taskDescriptions: TaskDescription[] = [
+  //   { id: 1, description: 'Procrastinating ' },
+  //   { id: 2, description: 'Using compiling time as an excuse to slack off' },
+  //   { id: 3, description: 'Paid poop time' },
+  //   { id: 4, description: 'Inserting new bugs to ensure you\'ll always have a job' },
+  // ];
+  constructor(public timeTrackservice: TimeTrackService, public projectService: ProjectService,
+    public taskCategoryService: TaskCategoryService, public taskDescriptionService: TaskDescriptionService, public fb: FormBuilder) {
+    this.createFakeData();
+    this._model = new TimeTrack();
     this.group = fb.group({
-      
+
     });
   }
 
-  get model () {
+  get model() {
     return this._model
   };
-  
+
   ngOnInit(): void {
+
+  }
+  trackHours() {
+    this.timeTrackservice.addEntity(this._model);
+    this._model = new TimeTrack();
   }
 
+  createFakeData() {
+    if (!localStorage.taskdescriptions) { localStorage.taskdescriptions = `${JSON.stringify([{ id: 1, description: "Pair Programming " }, { id: 2, description: "Meeting" }, { id: 3, description: "Other" }])}`; }
+    if (!localStorage.taskcategorys) { localStorage.taskcategorys = `${JSON.stringify([{ id: 1, description: "Software Development" }, { id: 2, description: "Project Management" }, { id: 3, description: "Other" }])}`; }
+    if (!localStorage.projects) { localStorage.projects = `[${JSON.stringify([{ id: 1, description: "Udemy" }, { id: 2, description: "Pinterest" }, { id: 3, description: "Google" }])}]` }
+
+  }
 }
