6c2cd98736130f777c5e04d1744ef9fc6a0a93ef
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-06 01:19:32 -0300

    - definindo focal points - adicionando toastr e mensagens de erro e sucesso - adicionando validação básica - criando mais dados fake para POC

--- src/app/members/private/timetracker/timetracker.component.ts
+++ src/app/members/private/timetracker/timetracker.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { TranslationWidth } from '@angular/common';
+import { ANALYZE_FOR_ENTRY_COMPONENTS, Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { ToastContainerDirective, ToastrService } from 'ngx-toastr';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { FocalPoint } from 'src/app/models/FocalPoint';
 import { Project } from 'src/app/models/Project';
@@ -18,6 +20,7 @@ import { TimeTrackService } from '../../../services/time-track.service';
   styleUrls: ['./timetracker.component.scss'],
 })
 export class TimeTrackerComponent implements OnInit {
+
   group: FormGroup;
   _model: TimeTrack;
   _focalPoints = new BehaviorSubject<Array<FocalPoint>>([]);
@@ -27,9 +30,10 @@ export class TimeTrackerComponent implements OnInit {
   //   { id: 3, description: 'Paid poop time' },
   //   { id: 4, description: 'Inserting new bugs to ensure you\'ll always have a job' },
   // ];
-  constructor(public timeTrackservice: TimeTrackService, public projectService: ProjectService,
+  constructor(private toastrService: ToastrService, public timeTrackservice: TimeTrackService, public projectService: ProjectService,
     public taskCategoryService: TaskCategoryService, public taskDescriptionService: TaskDescriptionService, public fb: FormBuilder) {
     this.createFakeData();
+    
     this._model = new TimeTrack();
     this.group = fb.group({
 
@@ -41,17 +45,32 @@ export class TimeTrackerComponent implements OnInit {
   };
 
   ngOnInit(): void {
-
+    this._focalPoints.next(JSON.parse(localStorage.focalpoints));
   }
   trackHours() {
-    this.timeTrackservice.addEntity(this._model);
-    this._model = new TimeTrack();
+    const isValid = this.validate();
+    if (isValid) {
+      this.timeTrackservice.addEntity(this._model);
+      this._model = new TimeTrack();
+      this.toastrService.success("Successfully saved!");
+      return;
+    }
+    this.toastrService.error("Please, check your data.");
+  }
+  validate() {
+    return this._model.date != null &&
+      this._model.focalPoint != null &&
+      this._model.minutes != null &&
+      this._model.project != null &&
+      this._model.taskCategory != null &&
+      this._model.taskDescription != null;
   }
 
   createFakeData() {
     if (!localStorage.taskdescriptions) { localStorage.taskdescriptions = `${JSON.stringify([{ id: 1, description: "Pair Programming " }, { id: 2, description: "Meeting" }, { id: 3, description: "Other" }])}`; }
     if (!localStorage.taskcategorys) { localStorage.taskcategorys = `${JSON.stringify([{ id: 1, description: "Software Development" }, { id: 2, description: "Project Management" }, { id: 3, description: "Other" }])}`; }
-    if (!localStorage.projects) { localStorage.projects = `[${JSON.stringify([{ id: 1, description: "Udemy" }, { id: 2, description: "Pinterest" }, { id: 3, description: "Google" }])}]` }
+    if (!localStorage.projects) { localStorage.projects = `${JSON.stringify([{ id: 1, description: "Udemy" }, { id: 2, description: "Pinterest" }, { id: 3, description: "Google" }])}` }
+    if (!localStorage.focalpoints) {localStorage.focalpoints = `${JSON.stringify([{ id: 1, name: "John Target" }, { id: 2, name: "Mary Aim" }, { id: 3, name: "Some Manager" }])}`;}
 
   }
 }
