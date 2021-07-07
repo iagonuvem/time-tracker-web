1126b7b86056332a540a00a03e4af1b7ae5831ed
Bruno Bemfica <bruno.bemfica@udemy.com>
2021-07-07 01:45:33 -0300

    componente login funcionando

--- src/app/members/public/login/login.component.ts
+++ src/app/members/public/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  _model = new User();
 
-  constructor() { }
+  constructor(private toastrService: ToastrService, private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  login(){
+    if(this._model.password != "1234567890" && this._model.email != "admin")
+    {
+      this.toastrService.error("Invalid username or password", "", {positionClass:"toast-top-right"});
+      
+    }
+    else
+    {
+      this.router.navigate(['/home']);
+    }
+    
+
+  }
 }
